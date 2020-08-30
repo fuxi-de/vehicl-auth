dee760385a18e352150f92d30b2081b0c84ce346
Flo <florian.schildwaechter@bauerxcel.de>
2020-08-30 14:47:39 +0200

    eslint

--- routes/users.js
+++ routes/users.js
@@ -1,38 +1,52 @@
 const mongoose = require('mongoose')
-const router = require('express').Router()   
+const router = require('express').Router()
 const User = mongoose.model('User')
 const passport = require('passport')
 const utils = require('../lib/utils')
 
-router.get('/protected', passport.authenticate('jwt', { session: false }), (req, res, next) => {
-  res.status(200).json({ success: true, msg: 'You are successfully authenticated to this route!'})
-})
+router.get(
+  '/protected',
+  passport.authenticate('jwt', { session: false }),
+  (req, res, next) => {
+    res
+      .status(200)
+      .json({
+        success: true,
+        msg: 'You are successfully authenticated to this route!',
+      })
+  }
+)
 
 // Validate an existing user and issue a JWT
-router.post('/login', function(req, res, next){
-
+router.post('/login', function (req, res, next) {
   User.findOne({ username: req.body.username })
     .then((user) => {
-
       if (!user) {
         res.status(401).json({ success: false, msg: 'could not find user' })
       }
-            
-      // Function defined at bottom of app.js
-      const isValid = utils.validPassword(req.body.password, user.hash, user.salt)
-            
-      if (isValid) {
 
+      // Function defined at bottom of app.js
+      const isValid = utils.validPassword(
+        req.body.password,
+        user.hash,
+        user.salt
+      )
+
+      if (isValid) {
         const tokenObject = utils.issueJWT(user)
 
-        res.status(200).json({ success: true, token: tokenObject.token, expiresIn: tokenObject.expires })
-
+        res
+          .status(200)
+          .json({
+            success: true,
+            token: tokenObject.token,
+            expiresIn: tokenObject.expires,
+          })
       } else {
-
-        res.status(401).json({ success: false, msg: 'you entered the wrong password' })
-
+        res
+          .status(401)
+          .json({ success: false, msg: 'you entered the wrong password' })
       }
-
     })
     .catch((err) => {
       next(err)
@@ -40,31 +54,25 @@ router.post('/login', function(req, res, next){
 })
 
 // Register a new user
-router.post('/register', function(req, res, next){
+router.post('/register', function (req, res, next) {
   const saltHash = utils.genPassword(req.body.pw)
-    
+
   const salt = saltHash.salt
   const hash = saltHash.hash
 
   const newUser = new User({
     username: req.body.uname,
     hash: hash,
-    salt: salt
+    salt: salt,
   })
 
   try {
-    
-    newUser.save()
-      .then((user) => {
-        res.json({ success: true, user: user })
-      })
-
+    newUser.save().then((user) => {
+      res.json({ success: true, user: user })
+    })
   } catch (err) {
-        
     res.json({ success: false, msg: err })
-    
   }
-
 })
 
 module.exports = router
